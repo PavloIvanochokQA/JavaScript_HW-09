187343dbf527fb3c2389ab2cf09ddc74511eb164
Pavlo-Ivanochok <ivanochokpavlo2@gmail.com>
2024-01-25 16:39:55 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
+};
 
+const startButtonRef = document.querySelector('[data-start]');
+const stopButtonRef = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+
+startButtonRef.addEventListener('click', onStartButtonClick);
+stopButtonRef.addEventListener('click', onStopButtonClick);
+stopButtonRef.disabled = true;
+let intervalId = null;
+
+function onStartButtonClick(event) {
+    event.target.disabled = true;
+    stopButtonRef.disabled = false;
+    intervalId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor()
+    }, 1000);
+};
+
+function onStopButtonClick(event) {
+    event.target.disabled = true;
+    startButtonRef.disabled = false;
+    clearInterval(intervalId);
+};
